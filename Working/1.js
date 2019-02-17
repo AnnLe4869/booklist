0288c6c51b542c71f2908a2f46197f5ab0acaa99
Anh Le <kunquan2345@gmail.com>
2019-02-17 15:23:28 -0800

    Use bubble to handle event

--- Working/1.js
+++ Working/1.js
@@ -1,3 +1,12 @@
-let liTag = document.querySelector(`li`).cloneNode(true);
-liTag.firstElementChild.textContent = `This is the book`;
-document.querySelector(`ul`).appendChild(liTag);
+// Use bubbling to handle event
+// Event will bubble up to the ancestor, trigger the ancestor handler
+// Key point: choose the ancestor and choose the element in which event happen
+// Make Javascript run faster and more efficent than assign event handler for each element
+// Take note on the difference between event.target and event.currentTarget
+let list = document.querySelector(`#book-list ul`);
+list.addEventListener("click", function(e) {
+  if (e.target.className === "delete") {
+    let li = e.target.parentElement;
+    list.removeChild(li);
+  }
+});
