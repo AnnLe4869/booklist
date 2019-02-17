b5463d5e2d4efd8609cb744d3ba0637d31603b14
Anh Le <kunquan2345@gmail.com>
2019-02-16 22:21:50 -0800

    Add new element with content and class

--- Working/1.js
+++ Working/1.js
@@ -1,13 +1,19 @@
-// A question here: why we still can modify the read-only value?
-let na = document.querySelector(`li`);
-na.firstElementChild.outerHTML = `<span class='name'>We are the world</span>`;
-na.lastElementChild.textContent = `This is the world class`;
-console.log(na.innerHTML);
-console.log(na.parentNode);
-na.parentNode.innerHTML += `
-                <li>
-                    <span class="name">Name of the Wind</span>
-                    <span class="delete">delete</span>
-                </li>
+function appendElement(spanContent) {
+  document.querySelector(`ul`).appendChild(document.createElement(`li`));
+  document
+    .querySelector(`ul`)
+    .lastElementChild.appendChild(createElement(`span`, spanContent, `name`));
+  document
+    .querySelector(`ul`)
+    .lastElementChild.appendChild(createElement(`span`, `Delete`, `delete`));
+}
 
-`;
+// Need a return value to pass as first parameter for appendChild() method
+function createElement(tag, content, className) {
+  let tagName = document.createElement(tag);
+  let elementContent = document.createTextNode(content);
+  tagName.appendChild(elementContent);
+  tagName.className = className;
+  return tagName;
+}
+appendElement("Hello the world");
