dacf5e5ad7e1f6194708b40578f34fbb4ff62a47
Anh Le <kunquan2345@gmail.com>
2019-02-17 02:32:12 -0800

    Use cloneNode() to add new element

--- Working/1.js
+++ Working/1.js
@@ -1,22 +1,3 @@
-// We use lastChildElement to navigate where to add child
-// Note that we still be able to modify the content inside a read-only property
-// Special note: pay attention to the return value of each method
-function appendElement(spanContent) {
-  document.querySelector(`ul`).appendChild(document.createElement(`li`));
-  document
-    .querySelector(`ul`)
-    .lastElementChild.appendChild(createElement(`span`, spanContent, `name`));
-  document
-    .querySelector(`ul`)
-    .lastElementChild.appendChild(createElement(`span`, `Delete`, `delete`));
-}
-
-// Need a return value to pass as first parameter for appendChild() method
-function createElement(tag, content, className) {
-  let tagName = document.createElement(tag);
-  let elementContent = document.createTextNode(content);
-  tagName.appendChild(elementContent);
-  tagName.className = className;
-  return tagName;
-}
-appendElement("Hello the world");
+let liTag = document.querySelector(`li`).cloneNode(true);
+liTag.firstElementChild.textContent = `This is the book`;
+document.querySelector(`ul`).appendChild(liTag);
