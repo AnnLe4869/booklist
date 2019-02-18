2e7c3d1e1bc9e5931c910b324371eee1b5679bd5
Anh Le <kunquan2345@gmail.com>
2019-02-17 22:37:02 -0800

    Second attempt to get input and add new element This time have clearing the input field after submiting

--- Working/1.js
+++ Working/1.js
@@ -1,3 +1,4 @@
+// This part is used to delete content
 let list = document.querySelector(`#book-list ul`);
 list.addEventListener(`click`, function(e) {
   if (e.target.className === `delete`) {
@@ -5,16 +6,21 @@ list.addEventListener(`click`, function(e) {
   }
 });
 
+// This part is used to add new content
 let addForm = document.forms[`add-book`];
 addForm.addEventListener(`submit`, function(e) {
+  // Default of submit is to reload the page, so without it the page will go back
+  // to stage before we input in ====> Necessary
   e.preventDefault();
   let formValue = this.querySelector(`input`).value;
-  console.log(formValue);
   addELement(formValue);
+  //Use this to clean the text field
+  this.reset();
 });
 
+// Need boolean value for cloneNode()
 function addELement(content) {
-  let newElement = document.querySelector(`li`).cloneNode();
+  let newElement = document.querySelector(`li`).cloneNode(true);
   newElement.firstElementChild.textContent = content;
   list.appendChild(newElement);
 }
