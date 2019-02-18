da6aebbd696ab883f1427b5d3a769c5637790523
Anh Le <kunquan2345@gmail.com>
2019-02-18 11:52:07 -0800

    Add checkbox to hide or show the content Using change event on checkbox

--- Working/1.js
+++ Working/1.js
@@ -1,4 +1,3 @@
-// This part is used to delete content
 let list = document.querySelector(`#book-list ul`);
 list.addEventListener(`click`, function(e) {
   if (e.target.className === `delete`) {
@@ -6,21 +5,27 @@ list.addEventListener(`click`, function(e) {
   }
 });
 
-// This part is used to add new content
 let addForm = document.forms[`add-book`];
 addForm.addEventListener(`submit`, function(e) {
-  // Default of submit is to reload the page, so without it the page will go back
-  // to stage before we input in ====> Necessary
   e.preventDefault();
   let formValue = this.querySelector(`input`).value;
   addELement(formValue);
-  //Use this to clean the text field
   this.reset();
 });
 
-// Need boolean value for cloneNode()
 function addELement(content) {
   let newElement = document.querySelector(`li`).cloneNode(true);
   newElement.firstElementChild.textContent = content;
   list.appendChild(newElement);
 }
+
+// Hide book
+// Here we use change event
+let hideBox = document.querySelector(`input[type='checkbox']`);
+hideBox.addEventListener("change", function(e) {
+  if (this.checked) {
+    list.style.display = "none";
+  } else {
+    list.style.display = "initial";
+  }
+});
