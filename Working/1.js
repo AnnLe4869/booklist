45aa50a74c38374a8b7d7a6fffaad81b887ca94c
Anh Le <kunquan2345@gmail.com>
2019-02-19 15:40:17 -0800

    Using dataset to toggle the content Note the use of dataset leverage information from Elements

--- Working/1.js
+++ Working/1.js
@@ -1,3 +1,4 @@
+// Remove book
 let list = document.querySelector(`#book-list ul`);
 list.addEventListener(`click`, function(e) {
   if (e.target.className === `delete`) {
@@ -5,22 +6,23 @@ list.addEventListener(`click`, function(e) {
   }
 });
 
+// Add new book
 let addForm = document.forms[`add-book`];
 addForm.addEventListener(`submit`, function(e) {
   e.preventDefault();
   let formValue = this.querySelector(`input[type='text']`).value;
   addELement(formValue);
   this.reset();
+
+  // Add new element
+  function addELement(content) {
+    let newElement = document.querySelector(`li`).cloneNode(true);
+    newElement.firstElementChild.textContent = content;
+    list.appendChild(newElement);
+  }
 });
 
-function addELement(content) {
-  let newElement = document.querySelector(`li`).cloneNode(true);
-  newElement.firstElementChild.textContent = content;
-  list.appendChild(newElement);
-}
-
 // Hide book
-// Here we use change event
 let hideBox = document.querySelector(`input[type='checkbox']`);
 hideBox.addEventListener("change", function(e) {
   if (this.checked) {
@@ -31,11 +33,7 @@ hideBox.addEventListener("change", function(e) {
 });
 
 // Custom search
-// We search for book in list
-// Pay attention to how to use 'input' event
 let searchBar = document.querySelector(`#search-books`).querySelector(`input`);
-// Can use either 'input' event or 'keyup' event
-// Need to lowercase to make searching easier
 searchBar.addEventListener("keyup", function(event) {
   [...list.children].forEach(ele => {
     if (
@@ -48,4 +46,24 @@ searchBar.addEventListener("keyup", function(event) {
       ele.style.display = "block";
     }
   });
+});
+
+// Tabs
+// Toggle content when click event is triggered
+// Using dataset to get more use from element
+let tabs = document.querySelector(`.tabs`);
+let panels = document.querySelectorAll(`.panel`);
+tabs.addEventListener(`click`, function(e) {
+  if (e.target.tagName == "LI") {
+    let targetPanel = document.querySelector(e.target.dataset.target);
+    console.log(targetPanel);
+    [...panels].forEach(panel => {
+      console.log(panel);
+      if (panel == targetPanel) {
+        panel.classList.add("active");
+      } else {
+        panel.classList.remove("active");
+      }
+    });
+  }
 });
