e84a4f477195479e6ee6e660112459850b52b498
Anh Le <kunquan2345@gmail.com>
2019-02-18 16:08:44 -0800

    Make custom search using 'keyup' event Can use 'input' event instead, though note how to use it Attention to what to choose: <form> or <input> explicitly

--- Working/1.js
+++ Working/1.js
@@ -8,7 +8,7 @@ list.addEventListener(`click`, function(e) {
 let addForm = document.forms[`add-book`];
 addForm.addEventListener(`submit`, function(e) {
   e.preventDefault();
-  let formValue = this.querySelector(`input`).value;
+  let formValue = this.querySelector(`input[type='text']`).value;
   addELement(formValue);
   this.reset();
 });
@@ -28,4 +28,24 @@ hideBox.addEventListener("change", function(e) {
   } else {
     list.style.display = "initial";
   }
+});
+
+// Custom search
+// We search for book in list
+// Pay attention to how to use 'input' event
+let searchBar = document.querySelector(`#search-books`).querySelector(`input`);
+// Can use either 'input' event or 'keyup' event
+// Need to lowercase to make searching easier
+searchBar.addEventListener("keyup", function(event) {
+  [...list.children].forEach(ele => {
+    if (
+      ele.firstElementChild.textContent
+        .toLowerCase()
+        .indexOf(this.value.toLowerCase()) === -1
+    ) {
+      ele.style.display = "none";
+    } else {
+      ele.style.display = "block";
+    }
+  });
 });
