b4fff0973fe50ce427059f10d2508d2b91b4df0b
Anh Le <kunquan2345@gmail.com>
2019-03-30 20:01:18 -0700

    Add jquery CDN and remove bootstrap link

--- Working/1.js
+++ Working/1.js
@@ -1,13 +1,1 @@
-//** Check the difference between onclick and adEventListener */
-document.querySelector(`#book-list h2`).onclick = changeContent;
-let changeElement = document.getElementById("content");
-changeElement.addEventListener(`click`, changeContent);
-changeElement.onclick = changeContent;
-//** Check the difference between content and content.target */
-//** One is the event object for click event, the second one is the target element,
-//   In other word, the element where the event fired
-function changeContent(content) {
-  content.target.textContent = `This is text change content`;
-  console.log(content);
-  console.log(content.target);
-}
+import "jquery";
